30cfaf1eed34aeaa4f98c916028d10973a055475
Jed Schmidt <tr@nslator.jp>
2012-08-27 21:42:43 +0900

    add tests

---
+++ test.js
@@ -0,0 +1,48 @@
+var assert = require("assert")
+var Router = require("./")
+
+var router = new Router({
+  "/": {
+    GET: function(req, res) {
+      res.writeHead(200)
+      res.end("OK")
+    }
+  }
+})
+
+router.route("/nomethod", function(req, res) {
+  res.writeHead(200)
+  res.end("OK")
+})
+
+router.onrequest(
+  {url: "/", method: "GET"},
+  {
+    writeHead: function(code){ assert.equal(code, 200) },
+    end: function(data){ assert.equal(data, "OK") }
+  }
+)
+
+router.onrequest(
+  {url: "/", method: "PATCH"},
+  {
+    writeHead: function(code){ assert.equal(code, 405) },
+    end: function(data){ assert.equal(data, "Method not allowed") }
+  }
+)
+
+router.onrequest(
+  {url: "/notfound", method: "GET"},
+  {
+    writeHead: function(code){ assert.equal(code, 404) },
+    end: function(data){ assert.equal(data, "Not found") }
+  }
+)
+
+router.onrequest(
+  {url: "/nomethod", method: "PATCH"},
+  {
+    writeHead: function(code){ assert.equal(code, 200) },
+    end: function(data){ assert.equal(data, "OK") }
+  }
+)
